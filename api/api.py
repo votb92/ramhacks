f86b247f39c27408a39b4cba6090f5c4cf752f05
votb92 <votb@mymail.vcu.edu>
2020-09-26 17:50:48 -0400

    login and signup

--- api/api.py
+++ api/api.py
@@ -13,3 +13,19 @@ def getZip():
     req_Json = request.json
     zip = req_Json['zip']
     return jsonify({"zipCode": zip})
+
+@app.route('/login', methods=['POST'])
+def getLogIn():
+    req_Json = request.json
+    userName = req_Json['userName']
+    password = req_Json['password']
+    return jsonify({"userName": userName, "password": password})
+
+
+@app.route('/signup', methods=['POST'])
+def getSignup():
+    req_Json = request.json
+    userName = req_Json['userName']
+    password = req_Json['password']
+    confirmPassword = req_Json['confirmPassword']
+    return jsonify({"userName": userName, "password": password, "confirmPassword":confirmPassword})
