473cb522b626a5f52f28d1c30252b115035f340f
votb92 <votb@mymail.vcu.edu>
2020-09-26 19:44:12 -0400

    create-login-resetPW functions

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ import pyrebase
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-
+from getpass import getpass
 firebaseConfig = {'apiKey': "AIzaSyByvfhbKg2soP06lWoAQNUkicTF6Yfw2rc",
                   'authDomain': "ramhacks-196ad.firebaseapp.com",
                   'databaseURL': "https://ramhacks-196ad.firebaseio.com",
@@ -23,6 +23,24 @@ db = firestore.client()
 # Database reference to stores
 stores_ref = db.collection(u'stores')
 docs = stores_ref.stream()
+
+# authentication
+auth = fb.auth()
+email = 'kurogane.deviruchi@gmail.com'
+password = 'ijetrnhd43!!3FGER!'
+
+#createUser()
+def CreateUser() :
+    global user
+    user = auth.create_user_with_email_and_password(email, password)
+
+#LogIn()
+def LogIn() :
+    login = auth.sign_in_with_email_and_password(email, password)
+
+#ResetPassword()
+def ResetPassword() :
+    auth.send_password_reset_email(email)
 
 stores = {}
 for doc in docs:
