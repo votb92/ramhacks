6581a633f934c2c3464f62a422a7caf8029f08f6
Lawangin Khan <lawangin.khan@gmail.com>
2020-09-27 04:41:00 -0400

    added card

--- client/src/Components/Layout/Body/index.js
+++ client/src/Components/Layout/Body/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ExampleCard } from '../../Card'
 
-const LayoutDiv = styled.div`
+const LayoutDivLeft = styled.div`
   height: 100vh;
   width: 50vw;
   border-width: 2px;
   border-color: red;
   background-color: blue;
+`
+const LayoutDivRight = styled.div`
+  height: 100vh;
+  width: 50vw;
+  border-width: 2px;
+  border-color: red;
+  background-color: green;
+`
+const ParentDiv = styled.div`
   display: flex;
 `
 
 export const Body = props => {
   return (
-    <div>
-      <LayoutDiv></LayoutDiv>
-      <LayoutDiv></LayoutDiv>
-    </div>
+    <ParentDiv>
+      <LayoutDivLeft>
+        <ExampleCard />
+      </LayoutDivLeft>
+      <LayoutDivRight></LayoutDivRight>
+    </ParentDiv>
   )
 }
