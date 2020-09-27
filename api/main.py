7a500261adbab0a8dc3c9143ee8046dd9d526b05
MusBansal <muskan.bansal@gmail.com>
2020-09-26 21:16:35 -0400

    Error in Extracting Miles from Api

--- api/main.py
+++ api/main.py
@@ -52,3 +52,27 @@ for address in stores_address:
     print(address)
 
 # print(stores)
+
+
+############Error#############
+import requests,json
+import smtplib
+
+#API KEY
+#api_file= ("api-key.txt", "r")
+api_key= "AIzaSyB2zq4lAoXmUL-vRSmJH7uRhvOsIsBRLgk"
+
+userLocation= input("Enter user address\n")
+storeLocation=input("enter store address\n")
+
+url="https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
+r=requests.get(url + "origins=" + userLocation + "&destinations=" +storeLocation +"&key=" +api_key)
+
+if r.status_code==200:
+    data = json.loads(r.text)
+    miles= data(['rows'][0]['elements']['distance']['text']
+    seconds= data(["rows"][0]["elements"][0]["duration"]["value"])
+#Print total time
+print("\nThe total travel distance from user to car store is ", miles)
+#print(data)
+
