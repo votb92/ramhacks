fd16a442d07022d79fce5b421796401220e978db
Lawangin Khan <lawangin.khan@gmail.com>
2020-09-27 03:41:54 -0400

    layout for body

--- client/src/Components/Layout/Body/index.js
+++ client/src/Components/Layout/Body/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledButton = styled.button`
-  background-color: red;
-  color: white;
+const LayoutDiv = styled.div`
+  height: 100vh;
+  width: 50vw;
+  border-width: 2px;
+  border-color: red;
+  background-color: blue;
+  display: flex;
 `
 
 export const Body = props => {
   return (
     <div>
-      <StyledButton>
-        Click Here
-      </StyledButton>
+      <LayoutDiv></LayoutDiv>
+      <LayoutDiv></LayoutDiv>
     </div>
   )
 }
