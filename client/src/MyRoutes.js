41d5c94cb490c7abaa83d2cb206b392b84d9a632
Lawangin Khan <lawangin.khan@gmail.com>
2020-09-27 07:08:29 -0400

    added zip screen

--- client/src/MyRoutes.js
+++ client/src/MyRoutes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { AuthScreen } from './Components/Layout/AuthScreen'
+import { ZipScreen} from './Components/Layout/ZipScreen'
 
 export const MyRoutes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={AuthScreen} />
+        <Route exact path='/zip' component={ZipScreen} />
       </Switch>
     </Router>
   )
