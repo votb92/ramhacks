0317ed934488f2d50cf3baf298773b4c490923fd
Lawangin Khan <lawangin.khan@gmail.com>
2020-09-26 21:37:34 -0400

    google maps fix

--- api/main.py
+++ api/main.py
@@ -2,6 +2,8 @@ import pyrebase
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import requests,json
+import smtplib
 
 firebaseConfig = {'apiKey': "AIzaSyByvfhbKg2soP06lWoAQNUkicTF6Yfw2rc",
                   'authDomain': "ramhacks-196ad.firebaseapp.com",
@@ -55,24 +57,23 @@ for address in stores_address:
 
 
 ############Error#############
-import requests,json
-import smtplib
 
 #API KEY
 #api_file= ("api-key.txt", "r")
-api_key= "AIzaSyB2zq4lAoXmUL-vRSmJH7uRhvOsIsBRLgk"
+api_key = "AIzaSyB2zq4lAoXmUL-vRSmJH7uRhvOsIsBRLgk"
 
-userLocation= input("Enter user address\n")
-storeLocation=input("enter store address\n")
+userLocation = input("Enter user address\n")
+storeLocation =input("enter store address\n")
 
 url="https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
 r=requests.get(url + "origins=" + userLocation + "&destinations=" +storeLocation +"&key=" +api_key)
 
-if r.status_code==200:
+if r.status_code == 200:
     data = json.loads(r.text)
-    miles= data(['rows'][0]['elements']['distance']['text']
-    seconds= data(["rows"][0]["elements"][0]["duration"]["value"])
+    miles = data['rows'][0]['elements'][0]['distance']['text']
+    seconds = data["rows"][0]["elements"][0]["duration"]["value"]
+    print("\nThe total travel distance from user to car store is ", miles)
+
 #Print total time
-print("\nThe total travel distance from user to car store is ", miles)
 #print(data)
 
