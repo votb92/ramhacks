6e5633a86273fdbaf44654c2a2abba53059e1c7f
Lawangin Khan <lawangin.khan@gmail.com>
2020-09-27 02:03:17 -0400

    fixed bugs in fee

--- api/main.py
+++ api/main.py
@@ -2,8 +2,7 @@ import pyrebase
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import requests,json
-import smtplib
+import requests, json
 import re
 
 firebaseConfig = {'apiKey': "AIzaSyByvfhbKg2soP06lWoAQNUkicTF6Yfw2rc",
@@ -42,14 +41,11 @@ for doc in docs:
     stores_cars.append({doc.id: single_store['cars']})
     stores_address.append({doc.id: single_store['address']})
 
-
 for location in stores_locations:
     print(location)
 
-
 for cars in stores_cars:
     print(cars)
-
 
 for address in stores_address:
     print(address)
@@ -57,15 +53,18 @@ for address in stores_address:
 # print(stores)
 
 
-#API KEY
-#api_file= ("api-key.txt", "r")
+# API KEY
+# api_file= ("api-key.txt", "r")
 api_key = "AIzaSyB2zq4lAoXmUL-vRSmJH7uRhvOsIsBRLgk"
 
 userLocation = input("Enter user address\n")
-storeLocation =input("enter store address\n")
+storeLocation = input("enter store address\n")
 
-url="https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
-r=requests.get(url + "origins=" + userLocation + "&destinations=" +storeLocation +"&key=" +api_key)
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
+r = requests.get(url + "origins=" + userLocation + "&destinations=" + storeLocation + "&key=" + api_key)
+
+miles = ''
+second = ''
 
 if r.status_code == 200:
     data = json.loads(r.text)
@@ -73,32 +72,27 @@ if r.status_code == 200:
     seconds = data["rows"][0]["elements"][0]["duration"]["value"]
     print("\nThe total travel distance from user to car store is ", miles)
 
-#Print total time
-#print(data)
-
 ### Calculations for Transfer Fee
 ## Notes: Algorithm- Distance of User to Dealership in Miles Divided by Distnace of US-20
 ## US Route 20 is the longest road in America going from Boston MA to Newport Oregon
 ##US Route_20 Distance- 3365 miles
 ## No parameters were provided by CarMax, so this equation is written to be a direct calculation and takes into account no factors ie. interstate shipping regulations, inflation, etc.
 
-#here is a dummy variable miles
-miles= 1234
+# Assume we take the variable directly from miles variable
+distance = re.sub(r'[^\d]', '', miles)
 
-#Assume we take the variable directly from miles variable
-distance= re.sub(r'[^\D]', '',miles)
 
 def roundtens(x):
-    return int(round(x/10.0))*10
+    return int(round(x / 10.0)) * 10
 
-#Algorithm
-route_20_distance= 3365
-transfer_cost= distance/route_20_distance *999
-transfer_cost=roundtens(transfer_cost)-.01
+
+# Algorithm
+route_20_distance = 3365
+transfer_cost = int(distance) / route_20_distance * 999
+transfer_cost = roundtens(transfer_cost) - .01
 
 if transfer_cost < 150:
-    transfer_cost=0
-    Print(transfer_cost)
+    transfer_cost = 0
+    print(transfer_cost)
 else:
     print(transfer_cost)
-
